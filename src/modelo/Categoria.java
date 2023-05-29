e0d6f40568bab2be1fbd979bef0ddb77c68f7168
Haynan Penha <haynan.mesquita@gmail.com>
2023-05-28 22:16:30 -0300

    Criando diversos testes

---
+++ src/modelo/Categoria.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
